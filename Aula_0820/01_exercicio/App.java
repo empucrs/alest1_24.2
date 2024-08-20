8e93645abc919de7d082df5bab7e34d8a7a6ba39
Edson Ifarraguirre Moreno <10083346@facin.portoalegre.pucrsnet.br>
2024-08-20 11:12:13 -0300

    Material inicial da disciplina

---
+++ Aula_0820/01_exercicio/App.java
@@ -0,0 +1,21 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        if(args.length==0)
+            System.out.println("Nada foi informado");
+        else
+            for(String txt: args)
+                System.out.println(txt);
+
+        if(args.length!=1){
+            System.out.println("É esperado o envio de um inteiro");
+            System.exit();
+        }
+
+        int tamVet = 
+        
+        
+    }
+    
+}
