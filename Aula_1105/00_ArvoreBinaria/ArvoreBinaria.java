4ddad283583fb26a854a8b80b8a8603ed1a9ac65
Edson Ifarraguirre Moreno <10083346@facin.portoalegre.pucrsnet.br>
2024-11-05 10:08:14 -0300

    Inicio da implementacao da arvore binaria

---
+++ Aula_1105/00_ArvoreBinaria/ArvoreBinaria.java
@@ -0,0 +1,23 @@
+public class ArvoreBinaria{
+    //public boolean addRoot(Integer e){}
+    //public boolean addLeft(Integer e, Integer father){}
+    //public boolean addRight(Integer e, Integer father){}
+    //public boolean hasRight(Integer element){}
+    //public boolean hasLeft(Integer element){}
+    //public Integer getParent(Integer e){}
+    //public boolean isInternal(Integer e){}
+    //public boolean isExternal(Integer e){}
+    //public boolean removeBranch(Integer e){}
+    //public boolean contains(Integer e){}
+    //public boolean isEmpty(){}
+    //public void clear(){}
+    //public int size(){}
+    //public Integer getRoot(){}
+    //public void setRoot(Integer e){}
+    //public Integer getLeft(Integer e){}
+    //public Integer getRight(Integer e){}
+    //public List<Integer> positionsPre(){}
+    //public List<Integer> positionsCentral(){}
+    //public List<Integer> positionsPos(){}
+    //public List<Integer> positionsWidth(){}
+}
